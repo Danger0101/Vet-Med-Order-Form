45f4fd7cda8d8935231bb477bf1bb607752879bf
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-02-13 21:05:17 +0000

    fix typo

--- vet-med-order-App/vite.config.js
+++ vet-med-order-App/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Vet-Med-Order-Form/", // This is needed for GitHub Pages
+  base: "/Vet-Med-Order-Form/",
 });
