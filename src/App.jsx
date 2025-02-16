4e8472376bdc68c249492dda83a5937f4d0e8ece
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-02-16 12:13:35 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./css/App.css";
 
 function App() {
   return (
-    <BrowserRouter basename="/Vet-Med-Order-Form">
+    <BrowserRouter basename="/">
       <div className="app-container">
         <h1>Vet Med Order Form</h1>
         <Routes>
